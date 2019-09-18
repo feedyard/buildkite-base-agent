cac3c300ac65b2208fce98f3df173850960af239
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-09-17 19:06:29 -0500

    initial commit

---
+++ hooks/environment.sh
@@ -0,0 +1,12 @@
+set -eu
+echo 'agent environment hook'
+
+# setup credentials to read environment config
+mkdir -p ~/.aws
+
+cat <<EOF > ~/.aws/credentials
+[default]
+aws_access_key_id=${VAL1}
+aws_secret_access_key=${VAL2}
+region=us-east-1
+EOF
---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import task
+
+@task
+def buildlocal(ctx):
+    ctx.run('docker build -t local/buildkite-base-agent:latest .')
+
+@task
+def testlocal(ctx):
+    ctx.run("bash testlocal.sh")
+
+# export BUILDKITE_AGENT_TOKEN= value from Buildkite
+@task
+def runlocalagent(ctx):
+    LAUNCH = "docker run -v \"/var/run/docker.sock:/var/run/docker.sock\" " \
+             "-e VAL1 " \
+             "-e VAL2 " \
+             "-d -t local/buildkite-base-agent:latest start " \
+             "--token $BUILDKITE_AGENT_TOKEN " \
+             "--tags \"queue=bootstrap\" "
+
+    ctx.run(LAUNCH)
